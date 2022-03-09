0758d16ab7758b5b1fbc6595a7c11bc2299839f1
Thibb1 <thibault2.brumaire@epitech.eu>
2022-03-09 15:04:21 +0100

    * opt fix

--- nm/src/main.c
+++ nm/src/main.c
@@ -9,9 +9,14 @@
 
 int main(int ac, char **av)
 {
+    int a = ac;
     if (ac == 1)
         return my_nm("a.out", 1);
-    for (int i = 1; i < ac; i++)
-        my_nm(av[i], ac == 2);
+    for (int i = 1; i < ac; i++) {
+        if (av[i][0] != '-')
+            my_nm(av[i], a == 2);
+        else
+            a--;
+    }
     return 0;
 }
